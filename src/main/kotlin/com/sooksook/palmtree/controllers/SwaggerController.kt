2169bd88ead63640356f13ebe67dbf2726172985
michael.10 <michael.10@kakaocorp.com>
2019-09-28 22:49:20 +0900

    Initialize the SwaggerController.

---
+++ src/main/kotlin/com/sooksook/palmtree/controllers/SwaggerController.kt
@@ -0,0 +1,12 @@
+package com.sooksook.palmtree.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class SwaggerController {
+    @GetMapping("/swagger")
+    fun swagger(): String {
+        return "swagger-ui.html"
+    }
+}
