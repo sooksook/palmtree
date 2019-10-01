cb6cd02f714c66f0ce79887f42d8d266bb9a46a1
michael.10 <michael.10@kakaocorp.com>
2019-10-01 09:54:21 +0900

    Join plant data to favorite results.

--- src/main/kotlin/com/sooksook/palmtree/controllers/FavoriteController.kt
+++ src/main/kotlin/com/sooksook/palmtree/controllers/FavoriteController.kt
@@ -4,6 +4,7 @@ import com.sooksook.palmtree.model.Favorite
 import com.sooksook.palmtree.model.FavoriteId
 import com.sooksook.palmtree.model.PlantId
 import com.sooksook.palmtree.repository.FavoriteRepository
+import com.sooksook.palmtree.repository.PlantRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
@@ -15,11 +16,23 @@ class FavoriteController {
     @Autowired
     lateinit var repository: FavoriteRepository
 
-    @CrossOrigin
     @GetMapping("/favorites")
     fun favorites(
             @RequestParam userId: String
-    ): FavoritesView = FavoritesView(repository.findAllByUserId(userId))
+    ): FavoritesResult = FavoritesResult(
+            repository
+                    .findAllByUserId(userId)
+                    .map {
+                        val plant = PlantRepository.get(it.plantId)
+                        FavoritesView(
+                                userId = it.userId,
+                                plantId = it.plantId,
+                                createdAt = it.createdAt,
+                                plantName = plant.name,
+                                plantEngName = plant.engName,
+                                imageUrl = plant.imageUrl
+                        )
+                    })
 
     @PostMapping("/favorites", consumes = [MediaType.APPLICATION_JSON_UTF8_VALUE])
     fun setFavorite(
@@ -56,8 +69,17 @@ class FavoriteController {
 
     data class JsonResult(val status: Int = 200)
 
-    data class FavoritesView(
-            val favorites: List<Favorite>,
+    data class FavoritesResult(
+            val favorites: List<FavoritesView>,
             val status: Int = 200
+    )
+
+    data class FavoritesView(
+            val userId: String,
+            val plantId: PlantId,
+            val createdAt: LocalDateTime,
+            val plantName: String,
+            val plantEngName: String,
+            val imageUrl: String
     )
 }
